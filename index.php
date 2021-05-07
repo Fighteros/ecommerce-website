a86c61756161c70d14637ed02dff6c72088e00cd
fighteros <am5419@fayoum.edu.eg>
2021-05-07 07:00:06 +0200

    finished Top

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--  bootstrap && fontawesome  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="styles/Main.css">
+    <title> Fighter_Os Store</title>
+</head>
+
+<body>
+    <!-- Top -->
+    <div id="top" class="top">
+        <div class="container">
+            <div class="col-md-6 offer">
+                <a href="#" class="btn btn-success btn-sm">Welcome</a>
+                <a href="checkout.php">4 Items In Your Cart | Total Price: $300</a>
+            </div>
+            <div class="md-col-6">
+                <ul class="menu">
+                    <li>
+                        <a href="customer_register.php">Register</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">My Account</a>
+                    </li>
+                    <li>
+                        <a href="cart.php">Go To Cart</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- JS Vendor Bootstrap JS && jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
+        integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
